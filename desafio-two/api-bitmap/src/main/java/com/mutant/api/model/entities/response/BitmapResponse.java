d3c6686276035b9f94fd8abfae575ee2e91a2187
740fernando <fernando.luiz92@hotmail.com>
2022-09-05 07:57:23 -0300

    include serialzable

--- desafio-two/api-bitmap/src/main/java/com/mutant/api/model/entities/response/BitmapResponse.java
+++ desafio-two/api-bitmap/src/main/java/com/mutant/api/model/entities/response/BitmapResponse.java
@@ -3,9 +3,12 @@ package com.mutant.api.model.entities.response;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class BitmapResponse {
+public class BitmapResponse implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private int[][] imageBitmap;
 }
