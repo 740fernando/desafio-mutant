c4310e9571934047b7c06601ab6b842345103239
740fernando <fernando.luiz92@hotmail.com>
2022-09-05 08:01:42 -0300

    added ResourceNotFoundException

---
+++ desafio-two/api-bitmap/src/main/java/com/mutant/api/exception/handler/MyExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.mutant.api.exception.handler;
+
+import com.mutant.api.exception.ExceptionResponse;
+import com.mutant.api.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class MyExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadRequestException(
+            Exception ex,
+            WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(
+                        new Date(),
+                        ex.getMessage(),
+                        request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
