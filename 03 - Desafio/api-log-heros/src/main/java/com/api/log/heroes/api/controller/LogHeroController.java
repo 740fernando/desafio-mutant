43b346bb318c487db1d33106d9f3009f13c9fb16
Fernando Luiz de Souza Vieira <32016610+740fernando@users.noreply.github.com>
2022-09-08 22:12:08 -0300

    Feature/desafio three (#30)
    ajustes

--- 03 - Desafio/api-log-heros/src/main/java/com/api/log/heroes/api/controller/LogHeroController.java
+++ 03 - Desafio/api-log-heros/src/main/java/com/api/log/heroes/api/controller/LogHeroController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/log/hero/v1")
